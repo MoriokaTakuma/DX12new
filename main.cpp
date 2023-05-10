9f7aa4c351b1c4adea4bf6722d5568a7ef2ebbd7
Morioka <morioka0607space@gmail.com>
2023-05-10 19:21:58 +0900

    少々

--- main.cpp
+++ main.cpp
@@ -34,6 +34,27 @@ WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	DebugOutputFormatString("Show window test.");
 	getchar();
 	return 0;
+
+	//ウィンドウクラスの生成 & 登録
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;//コールバック関数の指定
+	w.lpszClassName = _T("DX12Sample");		 //アプリケーションクラス名（適当でよい）
+	w.hInstance = GetModuleHandle(nullptr);  //ハンドルの取得
+
+	RegisterClassEx(&w);  //アプリケーションクラス（ウィンドウクラスの指定をOSに伝える）
+	
+	RECT wrc = { 0,0,window_width,window_height };//ウィンドウサイズを決める
+	
+	//関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,     //クラス名指定
+		                     _T("DX12テスト"),    //タイトルバー
+		                     
+		)
 }
 
 
@@ -47,7 +68,6 @@ WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		PostQuitMessage(0);//osに対して「もうこのアプリは終わる」と伝える
 		return 0;
 	}
-	return
+	return DefWindowProc(hwnd, msg, wparam, lparam);//既定の処理を行う
 
-		DefWindowProc(hwnd, msg, wparam, lparam);//既定の処理を行う
 }
